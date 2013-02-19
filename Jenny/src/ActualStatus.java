7ba4181b98144f5d158b79282317ba22e26c2eda
olololwtfomg <olololwtfomg@gmail.com>
2013-02-19 22:45:34 +0100

    pridana funkcia calculate heuristics

--- Jenny/src/ActualStatus.java
+++ Jenny/src/ActualStatus.java
@@ -3,6 +3,8 @@ public class ActualStatus {
 	
 	interface StatusConsts{
 		public static final int SECTOR_SIZE=14;
+		public static final int HEUR_OFFSET=2;
+		public static final int HEUR_THRESHOLD=150;
 	}
 	public int side=0;
 	public int roundsToEnd = 0;
@@ -10,11 +12,26 @@ public class ActualStatus {
 	public Sector[][] battlefield = 
 			new Sector[StatusConsts.SECTOR_SIZE][StatusConsts.SECTOR_SIZE];
 	
-	/*
+	
 	public void calculateHeuristics()
 	{
-		for
+		int value;
+		for(int xAxis=StatusConsts.HEUR_OFFSET;
+				xAxis<(StatusConsts.SECTOR_SIZE-StatusConsts.HEUR_OFFSET);
+				xAxis++){
+			for(int yAxis=StatusConsts.HEUR_OFFSET;
+					yAxis<(StatusConsts.SECTOR_SIZE-StatusConsts.HEUR_OFFSET);
+					yAxis++){
+				
+			}
+		}
 	}
-	*/
+
+	private int calculateSectorHeuristics(int x, int y)
+	{
+		int value=0;
+		//for(int i)
+		return value;
+	}
 
 }
